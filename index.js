5af6b752a7e5dfc06f96b6ad9e1b40bbdb7587c6
chrisbuttery <info@chrisbuttery.com>
2014-04-22 21:46:39 +1000

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+var ripple = require('ripple');
+var each = require('each');
+var intervals = require('intervals');
+var template = require('./template.html');
+var suffixed = require('date-suffix');
+var month = require('date-month');
+var day = require('date-day');
+
+var Clock = ripple(template)
+  .use(each)
+  .use(intervals);
+
+// Hook for when the view is created. This fires
+// before the view is rendered
+Clock.mounted(function(){
+  setInterval(this.tick, 1000);
+});
+
+// Add interpolation filters
+Clock.filter({
+  pad: function(num){
+    return String( num < 10 ? '0' + num : num );
+  },
+  suffixedDate: function(val){
+    return suffixed(val);
+  },
+  month: function(val){
+    return month(val).full();
+  },
+  day: function(val){
+    return day(val).full();
+  }
+});
+
+// Every second this will get called and
+// update the current time
+Clock.prototype.tick = function(){
+  clock.set('time', new Date());
+};
+
+module.exports = Clock;
